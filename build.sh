a5d2306f34940106e6bd24b0a2c15e80f67e98b0
Hemant Bhanoo <151884+hbhanoo@users.noreply.github.com>
2021-04-15 05:45:13 -0500

    update to latest vips version

--- build.sh
+++ build.sh
@@ -3,10 +3,10 @@
 # set -x
 set -e
 
-VIPS_VERSION=8.10.5
+VIPS_VERSION=8.10.6
 docker build --progress plain --build-arg VIPS_VERSION=${VIPS_VERSION} -t libvips-heroku20 container # uncomment this for testing (if helpful)
-#docker build --no-cache --build-arg VIPS_VERSION=${VIPS_VERSION} -t libvips-heroku18 container
+#docker build --no-cache --build-arg VIPS_VERSION=${VIPS_VERSION} -t libvips-heroku20 container
 
 mkdir -p build
 
-docker run --rm -t -v $PWD/build:/data libvips-heroku18 sh -c 'cp -f /usr/src/vips/build/*.tar.gz /data/'
+docker run --rm -t -v $PWD/build:/data libvips-heroku20 sh -c 'cp -f /usr/src/vips/build/*.tar.gz /data/'
