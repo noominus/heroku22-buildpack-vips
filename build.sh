7ee8ac9a239e61d4690c5f56c85d9c58fb5f00f5
Hemant Bhanoo <151884+hbhanoo@users.noreply.github.com>
2022-11-21 11:38:42 -0800

    add imagemagick;

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@
 set -e
 
 VIPS_VERSION=8.13.3
-docker build --progress plain --build-arg VIPS_VERSION=${VIPS_VERSION} -t libvips-heroku20 container # uncomment this for testing (if helpful)
-#docker build --no-cache --build-arg VIPS_VERSION=${VIPS_VERSION} -t libvips-heroku20 container
+docker build --progress plain --build-arg VIPS_VERSION=${VIPS_VERSION} -t libvips-heroku22 container # uncomment this for testing (if helpful)
+#docker build --no-cache --build-arg VIPS_VERSION=${VIPS_VERSION} -t libvips-heroku22 container
 
 mkdir -p build
 
-docker run --rm -t -v $PWD/build:/data libvips-heroku20 sh -c 'cp -f /usr/src/vips/build/*.tar.gz /data/'
+docker run --rm -t -v $PWD/build:/data libvips-heroku22 sh -c 'cp -f /usr/src/vips/build/*.tar.gz /data/'
